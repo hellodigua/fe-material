1ce4a356907315dd288397962fb35874a873eebf
digua <hellodigua@gmail.com>
2020-05-27 00:56:23 +0800

    feat: 文字故障效果

---
+++ pages/css/breakdown.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <p class="text" :data-text="text">{{ text }}</p>
+  </div>
+</template>
+<script>
+export default {
+  pageName: '故障风格文字效果',
+  props: {},
+  data() {
+    return {
+      text: 'TikTok'
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  background: black;
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.text {
+  color: white;
+  font-size: 100px;
+  position: relative;
+  margin: 0 auto;
+  padding: 2px;
+}
+
+.text::before {
+  content: attr(data-text);
+  background: black;
+  position: absolute;
+  text-shadow: 2px 0 red;
+  left: -2px;
+  clip-path: inset(0 0 0 0);
+  animation: ani2 2s infinite linear alternate-reverse;
+}
+
+.text::after {
+  content: attr(data-text);
+  background: black;
+  position: absolute;
+  text-shadow: -2px 0 blue;
+  left: 2px;
+  clip-path: inset(0 0 0 0);
+  animation: ani1 2s infinite linear alternate-reverse;
+}
+
+@keyframes ani1 {
+  $steps: 20;
+  @for $i from 0 through $steps {
+    #{percentage($i*(1/$steps))} {
+      clip-path: inset(random(100) + px 0 random(100) + px 0);
+    }
+  }
+}
+
+@keyframes ani2 {
+  $steps: 20;
+  @for $i from 0 through $steps {
+    #{percentage($i*(1/$steps))} {
+      clip-path: inset(random(100) + px 0 random(100) + px 0);
+    }
+  }
+}
+</style>
